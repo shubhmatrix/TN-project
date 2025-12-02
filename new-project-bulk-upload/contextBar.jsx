ec194c5c9e431d4d0ef8655dd0732e677157be72
Shubham Shrivastava <97872752+shubhmatrix@users.noreply.github.com>
2025-12-02 22:23:38 +0000

    fix the css

--- new-project-bulk-upload/contextBar.jsx
+++ new-project-bulk-upload/contextBar.jsx
@@ -1,15 +1,30 @@
-const ContextBar = () => {
-    return (
-  <>
+"use client";
+
+import styles from "../../styles/ContextBar.module.css";
+
+interface ContextBarProps {
+  selectedJob: string;
+  selectedEquipment: string;
+  hasSaved: boolean;
+  header_title: string;
+}
+
+const ContextBar = ({
+  selectedJob,
+  selectedEquipment,
+  hasSaved,
+  header_title,
+}: ContextBarProps) => {
+  return (
     <div className={styles["page-content-container"]}>
       <div className={styles["content-container"]}>
         <div
-          className={styles["template_content"]}
+          className={styles["template-content"]}
           id="new_asset_document"
           style={{ overflowY: "auto" }}
         >
-
-          <div className={styles["header"]} id="context_filter_bar">
+          {/* Header */}
+          <div className={styles.header} id="context_filter_bar">
             <h1 className={styles["context-bar-header"]}>
               {header_title}
             </h1>
@@ -17,6 +32,7 @@ const ContextBar = () => {
 
           <div className={styles["header-divider"]}></div>
 
+          {/* Attributes */}
           <div className={styles["header-attributes"]}>
             <div>
               <dt className={styles["attribute-name"]}>Job Number</dt>
@@ -30,12 +46,53 @@ const ContextBar = () => {
                 {selectedJob || "Not Selected"}
               </dd>
             </div>
+
+            <div>
+              <dt className={styles["attribute-name"]}>Job Name</dt>
+              <dd
+                className={
+                  hasSaved
+                    ? styles["attribute-text"]
+                    : styles["attribute-text-unselected"]
+                }
+              >
+                {hasSaved
+                  ? "Longwood – El Dorado 345kV Line Rebuild"
+                  : "Not Selected"}
+              </dd>
+            </div>
+
+            <div>
+              <dt className={styles["attribute-name"]}>Equipment</dt>
+              <dd
+                className={
+                  selectedEquipment
+                    ? styles["attribute-text"]
+                    : styles["attribute-text-unselected"]
+                }
+              >
+                {selectedEquipment || "Not Selected"}
+              </dd>
+            </div>
+          </div>
+
+          {/* Settings Button */}
+          <div className={styles.settingsContainer}>
+            <button
+              className="btn btn-outline-primary btn-sm"
+              id="btn-settings"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#reportSettings"
+            >
+              Settings
+            </button>
           </div>
 
         </div>
       </div>
     </div>
-  </>
-);
+  );
+};
 
-}
+export default ContextBar;
