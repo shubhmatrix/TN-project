6edcf5ee378d5cf381e7c14a8267f2266bbf4f72
Shubham Shrivastava <97872752+shubhmatrix@users.noreply.github.com>
2026-02-09 22:15:18 +0000

    code updated

--- API/Project management/VerifyCheckboxRenderer.tsx
+++ API/Project management/VerifyCheckboxRenderer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ICellRendererParams } from "ag-grid-community";
+import styles from "./VerifyCheckbox.module.css";
 
 interface Props extends ICellRendererParams {
   onClickVerify: (row: any) => void;
@@ -9,11 +10,14 @@ export default function VerifyCheckboxRenderer(props: Props) {
   const { data, onClickVerify } = props;
 
   return (
-    <input
-      type="checkbox"
-      checked={data.is_verified}
-      disabled={data.is_verified}
-      onChange={() => onClickVerify(data)}
-    />
+    <label className={styles.checkboxWrapper}>
+      <input
+        type="checkbox"
+        checked={data.is_verified}
+        disabled={data.is_verified}
+        onChange={() => onClickVerify(data)}
+      />
+      <span className={styles.customCheckbox} />
+    </label>
   );
 }
